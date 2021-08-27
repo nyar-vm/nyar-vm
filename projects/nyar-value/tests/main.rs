7df08f8e4956b9c17d67d415aff0b6041bc49240
Aster <galaster@foxmail.com>
2021-08-27 03:23:19 +0000

    🧪 Add new peg grammar

--- projects/nyar-value/tests/main.rs
+++ projects/nyar-value/tests/main.rs
@@ -2,3 +2,11 @@
 fn ready() {
     println!("it, works!")
 }
+
+#[test]
+#[ignore]
+fn peg_codegen() -> QResult {
+    let dir = current_dir()?.join("../valkyrie-parser/").canonicalize()?;
+    Compile::file(dir.join("src/parser/swarm.peg")).destination(dir.join("src/swarm.rs")).format().run().unwrap();
+    Ok(())
+}
