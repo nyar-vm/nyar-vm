e2090425dab99ad8a341e0258f28da58838cff3f
Aster <aster@vers.site>
2022-10-25 14:08:35 +0000

    🧪 Implement AliasExport for WasiResource

--- projects/wast-encoder/src/encoder/for_instance.rs
+++ projects/wast-encoder/src/encoder/for_instance.rs
@@ -1,5 +1,3 @@
-use crate::{Identifier, WasiModule};
-
 use super::*;
 
 impl ComponentDefine for CanonicalImport {
@@ -11,44 +9,13 @@ impl ComponentDefine for CanonicalImport {
     }
 }
 
-impl ComponentDefine for WasiInstance {
-    fn component_define<W: Write>(&self, w: &mut WastEncoder<W>) -> std::fmt::Result {
-        write!(w, "(import \"{name}\" (instance ${name}", name = self.module)?;
-        w.indent();
-        for wasi in self.resources.values() {
-            wasi.write_wasi_define(w)?;
-            w.newline()?;
-        }
-        for wasi in self.functions.values() {
-            w.export_function(wasi)?;
-            w.newline()?
-        }
-        w.dedent(2);
-        w.newline()?;
-        for (name, wasi) in &self.resources {
-            w.alias_export_resource(&self.module, wasi, name)?;
-            w.newline()?
-        }
-        for (name, wasi) in &self.functions {
-            w.alias_export_function(&self.module, wasi, name)?;
-            w.newline()?
-        }
-        Ok(())
-    }
-}
-
 impl<'a, W: Write> WastEncoder<'a, W> {
-    fn alias_export_resource(&mut self, module: &WasiModule, resource: &WasiResource, name: &Identifier) -> std::fmt::Result {
-        let id = name.wasi_id();
-        let name = resource.wasi_name.as_str();
-        write!(self, "(alias export ${module} \"{name}\" (type {id}))")
-    }
-    fn export_parameter(&mut self, input: &WasiParameter) -> std::fmt::Result {
+    pub(crate) fn export_parameter(&mut self, input: &WasiParameter) -> std::fmt::Result {
         write!(self, "(param \"{}\" ", input.wasi_name)?;
         input.r#type.write_wasi_reference(self)?;
         self.write_str(") ")
     }
-    fn export_function(&mut self, function: &ExternalFunction) -> std::fmt::Result {
+    pub(crate) fn export_function(&mut self, function: &ExternalFunction) -> std::fmt::Result {
         let name = function.wasi_name.as_str();
         write!(self, "(export \"{name}\" (func")?;
         self.indent();
@@ -61,16 +28,5 @@ impl<'a, W: Write> WastEncoder<'a, W> {
         }
         self.dedent(2);
         Ok(())
-    }
-    //     (alias export $wasi:io/streams@0.2.0 "[method]output-stream.blocking-write-and-flush" (func $output-stream.blocking-write-and-flush))
-    fn alias_export_function(
-        &mut self,
-        module: &WasiModule,
-        function: &ExternalFunction,
-        name: &Identifier,
-    ) -> std::fmt::Result {
-        let id = name.wasi_id();
-        let name = function.wasi_name.as_str();
-        write!(self, "(alias export ${module} \"{name}\" (func {id}))")
     }
 }
