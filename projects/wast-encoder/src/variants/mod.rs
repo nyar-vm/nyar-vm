22b9b588d95a18d4b100b2438207d00744a86e1c
Aster <aster@vers.site>
2024-02-24 01:33:40 +0800

    Variant item may have no item

--- projects/wast-encoder/src/variants/mod.rs
+++ projects/wast-encoder/src/variants/mod.rs
@@ -1,35 +1,36 @@
-use std::{ops::AddAssign, sync::Arc};
+use std::{fmt::Display, ops::AddAssign, str::FromStr, sync::Arc};
 
 use convert_case::{Case, Casing};
 use indexmap::IndexMap;
 
-use crate::WasiType;
+use crate::{Identifier, WasiType};
 
 mod arithmetic;
 
 #[derive(Clone, Debug)]
-pub struct WasiVariant {
-    /// Variant language name
-    pub name: Arc<str>,
+pub struct VariantType {
+    /// Variant name in language
+    pub symbol: Identifier,
     pub wasi_name: String,
-    pub variants: IndexMap<Arc<str>, WasiVariantItem>,
+    pub variants: IndexMap<Arc<str>, VariantItem>,
 }
 
 #[derive(Clone, Debug)]
-pub struct WasiVariantItem {
-    pub name: Arc<str>,
+pub struct VariantItem {
+    /// Variant name in language
+    pub symbol: Arc<str>,
     pub wasi_name: String,
-    pub r#type: WasiType,
+    pub fields: Option<WasiType>,
 }
 
-impl WasiVariant {
-    pub fn new<S, T>(name: S) -> Self
+impl VariantType {
+    pub fn new<S>(name: S) -> Self
     where
-        S: Into<Arc<str>>,
+        S: Into<Identifier>,
     {
         let name = name.into();
-        let wasi_name = name.to_case(Case::Kebab);
-        Self { name, wasi_name: wasi_name.to_string(), variants: IndexMap::new() }
+        let wasi_name = name.wasi_name();
+        Self { symbol: name, wasi_name, variants: IndexMap::new() }
     }
     /// Custom wasi name for the variant type
     pub fn with_wasi_name<S>(self, wasi_name: S) -> Self
@@ -40,15 +41,14 @@ impl WasiVariant {
     }
 }
 
-impl WasiVariantItem {
-    pub fn new<S, T>(name: S, r#type: T) -> Self
+impl VariantItem {
+    pub fn new<S>(name: S) -> Self
     where
         S: Into<Arc<str>>,
-        T: Into<WasiType>,
     {
         let name = name.into();
-        let wasi_name = name.to_case(Case::Kebab);
-        Self { name: name.into(), wasi_name: wasi_name.to_string(), r#type }
+        let wasi_name = name.as_ref().to_case(Case::Kebab);
+        Self { symbol: name, wasi_name, fields: None }
     }
     /// Custom wasi name for the variant item
     pub fn with_wasi_name<S>(self, wasi_name: S) -> Self
@@ -56,6 +56,12 @@ impl WasiVariantItem {
         S: Into<String>,
     {
         Self { wasi_name: wasi_name.into(), ..self }
+    }
+    pub fn with_field<T>(self, field: T) -> Self
+    where
+        T: Into<WasiType>,
+    {
+        Self { r#type: field.into(), ..self }
     }
 }
 
