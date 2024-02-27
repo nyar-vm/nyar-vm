d170d985fb1a5eefefb5170ddaa74e794dfa8200
Aster <aster@vers.site>
2024-02-27 13:59:13 +0800

    Add extension function for wasmtime

---
+++ projects/wasm-interpreter/src/host/mod.rs
@@ -0,0 +1,55 @@
+use crate::wasi;
+
+pub struct NyarExtension {}
+
+impl wasi::debugger::print::Host for NyarExtension {
+    fn print_i8(&mut self, i: i8) -> anyhow::Result<()> {
+        println!("{}", i);
+        Ok(())
+    }
+
+    fn print_i16(&mut self, i: i16) -> anyhow::Result<()> {
+        println!("{}", i);
+        Ok(())
+    }
+
+    fn print_i32(&mut self, i: i32) -> anyhow::Result<()> {
+        println!("{}", i);
+        Ok(())
+    }
+
+    fn print_i64(&mut self, i: i64) -> anyhow::Result<()> {
+        println!("{}", i);
+        Ok(())
+    }
+
+    fn print_u8(&mut self, u: u8) -> anyhow::Result<()> {
+        println!("{}", u);
+        Ok(())
+    }
+
+    fn print_u16(&mut self, u: u16) -> anyhow::Result<()> {
+        println!("{}", u);
+        Ok(())
+    }
+
+    fn print_u32(&mut self, u: u32) -> anyhow::Result<()> {
+        println!("{}", u);
+        Ok(())
+    }
+
+    fn print_u64(&mut self, u: u64) -> anyhow::Result<()> {
+        println!("{}", u);
+        Ok(())
+    }
+
+    fn print_f32(&mut self, f: f32) -> anyhow::Result<()> {
+        println!("{}", f);
+        Ok(())
+    }
+
+    fn print_f64(&mut self, f: f64) -> anyhow::Result<()> {
+        println!("{}", f);
+        Ok(())
+    }
+}
--- projects/wasm-interpreter/tests/main.rs
+++ projects/wasm-interpreter/tests/main.rs
@@ -1,13 +1,9 @@
 use std::path::Path;
 
-use wasmtime_wasi::preview2::WasiView;
-
-mod host;
-mod vm;
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/component.wat");
-    crate::vm::build_vm(&path).await?;
+    let wast = std::fs::read_to_string(&path)?;
+    NyarVM::load_wast(&wast).await?;
     Ok(())
 }
