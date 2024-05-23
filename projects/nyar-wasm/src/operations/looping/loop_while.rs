a6ced91a2800fcdaece1ede7776437bc33a8e1d8
Aster <aster@vers.site>
2024-05-23 12:36:08 +0800

    Add loop instructions

---
+++ projects/nyar-wasm/src/operations/looping/loop_while.rs
@@ -0,0 +1,11 @@
+use super::*;
+impl Emit for LoopWhileBody {
+    fn emit<W: Write>(&self, w: &mut WastEncoder<W>) -> std::fmt::Result {
+        self.make_loop().emit(w)
+    }
+}
+impl LoopWhileBody {
+    pub fn make_loop(&self) -> LoopRepeat {
+        LoopRepeat { label: self.label.clone() }
+    }
+}
